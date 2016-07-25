f2175bfd4f65fd7cb685c263db263b882d9a0c53
Sal Espinosa <sespinos@gmail.com>
2016-07-25 11:03:15 -0600

    Add ability to log in as existing user

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
   resources :homes, only: [:show]
   resources :users, only: [:new, :create]
   resources :links, only: [:index]
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
